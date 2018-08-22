43f12bb6b521a4bc7907a0ef50edeba50b951bf4
Michel Felipe <mfelipeof@gmail.com>
2018-08-22 13:55:33 -0300

    Moved wordpress-tools Dockerfile to a github repo
    Pushed a docker image to docker hub/cloud, called by mfdeveloper/wordpress-tools

--- scripts/WpAutomation/Tests.php
+++ scripts/WpAutomation/Tests.php
@@ -7,11 +7,12 @@
     class Tests {
 
         protected static $baseDir = 'scripts/WpAutomation';
+        protected static $pluginsWithTests = [];
         
         public static function install(Event $event)
         {
-            $rootDir = str_replace(self::$baseDir , '', dirname(__FILE__));
-            $pluginsDir = new \DirectoryIterator($rootDir.'wp-content/plugins');
+            $rootDir = str_replace(self::$baseDir, '', dirname(__FILE__));
+            $pluginsDir = new \DirectoryIterator($rootDir . 'wp-content/plugins');
 
             foreach ($pluginsDir as $dir) {
                 if($dir->isDot()) continue;
@@ -21,8 +22,14 @@
                     echo 'Installing tests of: '.$pluginsDir->getBasename(). " plugin\n";
                     $command = $dir->getRealPath()."/bin/install-wp-tests.sh {$_ENV['DB_TEST_NAME']} root {$_ENV['DB_ROOT_PASSWORD']} {$_ENV['DB_TEST_HOST']}";
                     echo shell_exec($command);
+
+                    self::$pluginsWithTests[$dir->getBasename()] = true;
         
                 }
+            }
+
+            if ( count(self::$pluginsWithTests) === 0 ) {
+                echo "No scripts /bin/install-wp-tests.sh for install phpunit into plugins folder were found. Please, run: 'wp scaffold plugin-tests' before";
             }
         }
     }
